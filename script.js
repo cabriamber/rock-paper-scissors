c5262024bab3817b80f40fe29f783066fdc0bf96
Cabri Chamberlin <cabriamber@gmail.com>
2023-05-24 17:14:27 -0600

    Create variables to hold the current score

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const computerOptions = ["rock", "paper", "scissors"]
 // create an array representing possible computer choices 
 
-let userInput = prompt("Rock, Paper, or Scissors?"); 
-console.log(userInput);
+//let userInput = prompt("Rock, Paper, or Scissors?"); // prompt the user to make their choice 
+//console.log(userInput);
 
-let userChoice = userInput.toLowerCase(); 
-console.log(userChoice);
+//let userChoice = userInput.toLowerCase(); 
+//console.log("You chose " + userChoice); //coerce the user choice to all lower case 
 
 function getRandom() { //returns randomly either 0, 1 or 2
   let number = Math.floor(Math.random() * 3);
@@ -20,4 +20,40 @@ function getComputerChoice() {
   return choice; 
 }
 let computerChoice = getComputerChoice(); 
-//console.log(computerChoice);
+console.log("The computer chose " +computerChoice);
+
+let userWins = 0; 
+let computerWins = 0; 
+
+function getWinner() { //determine the winner of the round and display an appropriate message
+  if (computerChoice === userChoice) {
+    console.log("It's a tie! Try again!")
+  }
+  else if (computerChoice == "rock" && userChoice == "paper") {
+    console.log("Paper covers rock! You win!")
+    userWins = userWins +1; 
+  }
+  else if (computerChoice == "rock" && userChoice == "scissors") {
+    console.log("Rock smashes scissors! You lose!")
+    computerWins = computerWins +1; 
+  }
+  else if (computerChoice == "scissors" && userChoice == "paper") {
+    console.log("Scissors cut paper! You lose!")
+    computerWins = computerWins +1; 
+  }
+  else if (computerChoice == "scissors" && userChoice == "rock") {
+    console.log ("Rock smashes scissors! You win!")
+    userWins = userWins +1; 
+  }
+  else if (computerChoice == "paper" && userChoice == "rock") {
+    console.log("Paper covers rock! You lose!")
+    computerWins = computerWins +1; 
+  }
+  else if (computerChoice = "paper" && userChoice == "scissors") {
+    console.log("Scissors cut paper! You win!")
+    userWins = userWins +1; 
+  }
+
+}
+
+//let outcome = getWinner(); 
