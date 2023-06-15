4b629394beeae7cd2b21aa6a4c33bc129c9d91cd
Cabri Chamberlin <cabriamber@gmail.com>
2023-06-14 20:14:41 -0600

    Add loops to make the game play 5 rounds

--- script.js
+++ script.js
@@ -1,59 +1,101 @@
-const computerOptions = ["rock", "paper", "scissors"]
+let userWins = 0; 
+let computerWins = 0; 
+
 // create an array representing possible computer choices 
+let userChoice = 'y';
+//console.log(userChoice);
+function getUserChoice() { //when called prompt the user to make a choice and return the result 
+  let userInput = prompt("Rock, Paper, or Scissors?");
+  console.log(userInput);
+  let choice = userInput.toLowerCase(); 
+  console.log("You chose " + choice);
+  userChoice = choice; 
+  return choice; 
+}
 
-//let userInput = prompt("Rock, Paper, or Scissors?"); // prompt the user to make their choice 
-//console.log(userInput);
-
-//let userChoice = userInput.toLowerCase(); 
-//console.log("You chose " + userChoice); //coerce the user choice to all lower case 
 
 function getRandom() { //returns randomly either 0, 1 or 2
   let number = Math.floor(Math.random() * 3);
   return number;  
 }
-let x = getRandom()
-//console.log(x)
 
+let computerChoice = 'x';
+//console.log(computerChoice);
+const computerOptions = ["rock", "paper", "scissors"]
 function getComputerChoice() {
   let randomNumber = getRandom(); 
   let choice = computerOptions[randomNumber];
+  console.log("The computer chose " +choice);
+  computerChoice = choice; 
   return choice; 
 }
-let computerChoice = getComputerChoice(); 
-console.log("The computer chose " +computerChoice);
+//let x = getComputerChoice();
+//console.log(computerChoice)
 
-let userWins = 0; 
-let computerWins = 0; 
+//let computerChoice = getComputerChoice(); 
+//console.log("The computer chose " +computerChoice);
+
+
 
 function getWinner() { //determine the winner of the round and display an appropriate message
   if (computerChoice === userChoice) {
     console.log("It's a tie! Try again!")
+    return "It's a tie! Try again!";
   }
   else if (computerChoice == "rock" && userChoice == "paper") {
     console.log("Paper covers rock! You win!")
     userWins = userWins +1; 
+    return "Paper covers rock! You win!";
   }
   else if (computerChoice == "rock" && userChoice == "scissors") {
     console.log("Rock smashes scissors! You lose!")
     computerWins = computerWins +1; 
+    return "Rock smashes scissors! You lose!"; 
   }
   else if (computerChoice == "scissors" && userChoice == "paper") {
     console.log("Scissors cut paper! You lose!")
     computerWins = computerWins +1; 
+    return "Scissors cut paper! You lose!";
   }
   else if (computerChoice == "scissors" && userChoice == "rock") {
     console.log ("Rock smashes scissors! You win!")
     userWins = userWins +1; 
+    return "Rock smashes scissors! You win!";
   }
   else if (computerChoice == "paper" && userChoice == "rock") {
     console.log("Paper covers rock! You lose!")
     computerWins = computerWins +1; 
+    return "Paper covers rock! You lose!"; 
   }
   else if (computerChoice = "paper" && userChoice == "scissors") {
     console.log("Scissors cut paper! You win!")
     userWins = userWins +1; 
+    return "Scissors cut paper! You win!";
   }
 
 }
 
-//let outcome = getWinner(); 
+function playRound() {
+  let userChoice = getUserChoice(); 
+  let computerChoice = getComputerChoice();
+  let winner = getWinner(); 
+  console.log(winner); 
+  console.log("Computer "+ computerWins)
+  console.log("You " + userWins)
+
+}
+
+//let outcome = getWinner();
+
+let x = playRound();
+
+let rounds = 0; 
+
+function game() {
+  while (rounds < 5) {
+    playRound(); 
+    rounds = rounds +1; 
+  }
+}
+
+game(); 
